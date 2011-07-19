b98c9e9e62356f0b08fde56926d577847c0a1cc8
swenger <spwenger@googlemail.com>
2011-07-19 17:06:59 +0200

    gccxml attribute example

--- mytest.h
+++ mytest.h
@@ -1,6 +1,9 @@
 #ifndef _MYTEST_H
 #define _MYTEST_H
 
+#define SOME_CONSTANT 1
+#define _out_ __attribute((gccxml("out")))
+
 struct Point {
     float x;
     float y;
@@ -18,7 +21,7 @@ union Something {
 
 float area(const struct Rect *r);
 
-void rect_from_center(struct Rect *r, const struct Point *c, const float w, const float h);
+void rect_from_center(_out_ struct Rect *r, const struct Point *c, const float w, const float h);
 
 #endif
 
